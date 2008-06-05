3d164a86a6fa35f765b82f059dc0de8f86c21467
assaf <assaf@13f79535-47bb-0310-9956-ffa450edef68>
2008-06-05 23:26:31 +0000

    Fixed task view rendering, using SalesForce form as test case.
    git-svn-id: https://svn.apache.org/repos/asf/ode/sandbox/singleshot@663776 13f79535-47bb-0310-9956-ffa450edef68

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,32 +2,33 @@
 // This file is automatically included by javascript_include_tag :defaults
 //
 var Singleshot = {
-  // Returns the SingleShot.TaskView object.
-  taskView: function(target) {
+  setFrameSize: function(target) {
     target = $$(target).first() || $(target);
     // Adjust lower frame to expand and fit the reminder of the window.
     // Do it once now, and each time the window is resized.
     var adjust = function() {
-      target.style.height = window.innerHeight - target.offsetTop;
+      target.style.height = window.innerHeight - target.offsetTop + 'px';
     }
     Event.observe(window, 'resize', adjust);
-    adjust();
-    Singleshot.taskView = function() { };
+    Event.observe(window, 'load', adjust);
   },
 
+  // Called to show/hide (or expand/collapse) the target element on click.
   expand: function(event, target, alternative) {
     event = event || window.event;
     var source = Event.element(event);
     target = $$(target).first() || $(target);
     if (target.visible()) {
-      source.innerHTML = source.originalText; 
+      if (source.originalText)
+        source.innerHTML = source.originalText; 
       target.hide();
     } else if (event.shiftKey || event.ctrlKey || event.metaKey) {
       return;
     } else {
-      source.originalText = source.innerHTML;
-      if (alternative)
+      if (alternative) {
+        source.originalText = source.innerHTML;
         source.innerHTML = alternative;
+      }
       target.show();
     }
     Event.stop(event);
