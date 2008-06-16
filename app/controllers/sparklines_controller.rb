967ed3356191f60d71d38fe5efb7338cb0443863
Assaf Arkin <assaf@apache.org>
2008-06-16 02:16:48 +0000

    Added drop shadow for buttons, changed gray border for header, and add transparent background to sparklines.
    git-svn-id: https://svn.apache.org/repos/asf/ode/sandbox/singleshot@668037 13f79535-47bb-0310-9956-ffa450edef68

--- app/controllers/sparklines_controller.rb
+++ app/controllers/sparklines_controller.rb
@@ -2,9 +2,11 @@ class SparklinesController < ApplicationController
 
   skip_before_filter :authenticate
 
+  COLOR_SCHEME = { :above_color=>'#65a0e4', :below_color=>'#a8c0d8', :target=>50, :target_color=>'#ffffd0', :background_color=>'transparent' }
+
   def index
-    options = { :type=>'bar', :step=>4, :above_color=>'#65a0e4', :below_color=>'#a8c0d8', :target=>50, :target_color=>'#ffffd0' }
-    render :text=>Sparklines.plot(params[:results].split(',').map(&:to_f), options.merge(params)), :content_type=>'image/png'
+    options = COLOR_SCHEME.merge(:type=>'bar', :step=>4).merge(params)
+    render :text=>Sparklines.plot(params[:results].split(',').map(&:to_f), options), :content_type=>'image/png'
   end
 
 end
