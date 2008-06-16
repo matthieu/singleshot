b6eb7fbb899501c4261d477d76f461553954790e
Assaf Arkin <assaf@apache.org>
2008-06-16 02:16:44 +0000

    Feeds now link back to source page. Fixed bug in activity and completed sparklines. Activities feed limited to 50 entries, page to 2 days, graph to 1 month.
    git-svn-id: https://svn.apache.org/repos/asf/ode/sandbox/singleshot@668036 13f79535-47bb-0310-9956-ffa450edef68

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,8 @@ class TasksController < ApplicationController
 
   def index
     @title, @subtitle = 'Tasks', 'Tasks you are performing or can claim for your own.'
-    @alternate = { Mime::ATOM=>formatted_tasks_url(:format=>:atom, :access_key=>authenticated.access_key), 
+    @alternate = { Mime::HTML=>tasks_url,
+                   Mime::ATOM=>formatted_tasks_url(:format=>:atom, :access_key=>authenticated.access_key), 
                    Mime::ICS=>formatted_tasks_url(:format=>:ics, :access_key=>authenticated.access_key) }
     @tasks = Task.pending.for_stakeholder(authenticated).with_stakeholders.rank_for(authenticated)
     respond_to do |wants|
@@ -22,12 +23,13 @@ class TasksController < ApplicationController
 
   def completed
     @title, @subtitle = 'Completed', 'Completed tasks'
-    @alternate = { Mime::ATOM=>formatted_completed_tasks_url(:format=>:atom, :access_key=>authenticated.access_key), 
+    @alternate = { Mime::HTML=>completed_tasks_url,
+                   Mime::ATOM=>formatted_completed_tasks_url(:format=>:atom, :access_key=>authenticated.access_key), 
                    Mime::ICS=>formatted_completed_tasks_url(:format=>:ics, :access_key=>authenticated.access_key) }
     @tasks = Task.completed.for_stakeholder(authenticated).with_stakeholders
     respond_to do |wants|
       wants.html do
-        @graph = Task.connection.select_values("SELECT tasks.updated_at FROM tasks JOIN stakeholders ON stakeholders.task_id = tasks.id WHERE person_id=#{authenticated.id} AND role='owner' AND status='completed' AND tasks.updated_at >= '#{Date.current - 1.month}'").group_by { |date| date.to_date }.map { |date, entries| entries.size }
+        @graph = Task.connection.select_values("SELECT tasks.updated_at FROM tasks, stakeholders WHERE stakeholders.task_id = tasks.id AND person_id=#{authenticated.id} AND role='owner' AND status='completed' AND tasks.updated_at >= '#{Date.current - 1.month}'")
       end
       # TODO: wants.xml
       # TODO: wants.json
@@ -38,7 +40,8 @@ class TasksController < ApplicationController
 
   def following
     @title, @subtitle = 'Following', 'Tasks you created, observing or managing.'
-    @alternate = { Mime::ATOM=>formatted_following_tasks_url(:format=>:atom, :access_key=>authenticated.access_key), 
+    @alternate = { Mime::HTML=>following_tasks_url,
+                   Mime::ATOM=>formatted_following_tasks_url(:format=>:atom, :access_key=>authenticated.access_key), 
                    Mime::ICS=>formatted_following_tasks_url(:format=>:ics, :access_key=>authenticated.access_key) }
     @tasks = Task.following.for_stakeholder(authenticated).with_stakeholders
     respond_to do |wants|
@@ -52,7 +55,8 @@ class TasksController < ApplicationController
 
   def show
     @title = @task.title
-    @alternate = { Mime::ICS=>formatted_task_url(@task, :format=>:ics, :access_key=>authenticated.access_key),
+    @alternate = { Mime::HTML=>task_url(@task),
+                   Mime::ICS=>formatted_task_url(@task, :format=>:ics, :access_key=>authenticated.access_key),
                    Mime::ATOM=>formatted_task_activity_url(@task, :format=>:atom, :access_key=>authenticated.access_key) }
     respond_to do |wants|
       wants.html { render :layout=>'head' }
--- app/views/activity/index.atom.builder
+++ app/views/activity/index.atom.builder
@@ -1,4 +1,4 @@
-atom_feed :root_url=>activity_url do |feed|
+atom_feed :root_url=>@alternate[Mime::HTML] do |feed|
   feed.title @title
   feed.subtitle @subtitle
   feed.updated @activities.first.created_at unless @activities.empty?
@@ -9,7 +9,7 @@ atom_feed :root_url=>activity_url do |feed|
       entry.title activity_to_text(activity)
       entry.content :type=>'html' do |content|
         content.text! "<p>#{activity_to_html(activity)}</p>"
-        content.text!  truncate(strip_tags(task.description), 250)
+        content.text! truncate(strip_tags(task.description), 250)
       end
       entry.author do |author|
         author.name activity.person.fullname if activity.person
