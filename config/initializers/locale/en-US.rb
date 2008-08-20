5f7d519a3a46199c92acad07a8b304f18b6ca8ec
Assaf Arkin <assaf@apache.org>
2008-08-19 22:45:51 -0700

    I18n resources for activities

--- config/initializers/locale/en-US.rb
+++ config/initializers/locale/en-US.rb
@@ -1,8 +1,17 @@
 I18n.backend.store_translations :'en-US', {
-  :activities => {
+  :activity => {
     :index => {
       :title => "Activities",
       :subtitle => "Activity from all tasks you own or observe"
-    }            
+    },
+    :name => {
+      :created => "created",
+      :owner => "is owner of"
+    },
+    :entry => {
+      :text => "{{person}} {{names}} {{task}}",
+      :html => "{{person}} {{names}} {{task}}"
+    },
+    :sentence => "{{person}} {{list}} {{task}}"       
   }
 }
