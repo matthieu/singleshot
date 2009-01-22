a8c4dfdeef783803e4a756edb1b76e02964fc88a
Assaf Arkin <assaf@apache.org>
2009-01-22 14:30:07 -0800

    Added model annotation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,21 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with this
+# work for additional information regarding copyright ownership.  The ASF
+# licenses this file to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+
 # == Schema Information
-# Schema version: 20080621023051
+# Schema version: 20090121220044
 #
 # Table name: people
 #
@@ -11,8 +27,6 @@
 #  timezone   :integer(4)
 #  password   :string(64)
 #  access_key :string(32)      not null
-#  created_at :datetime
-#  updated_at :datetime
 #
 
 require 'sha1'
