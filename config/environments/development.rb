c95562a4d021fbe2e9a70edfa44a18c2e2eb5cae
Assaf Arkin <assaf@apache.org>
2009-02-10 00:32:49 -0800

    Moved gem dependencies to separate file config/gems.yml. That makes it easier to see all the dependencies at a glance, and also allows installing them all without loading config/environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,20 +20,3 @@ config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
-
-
-# These Gems are used exclusively during development/testing, they're listed here
-# to be included when running rake setup. Some of these are only used from rake
-# tasks or command line (e.g. rspec, thin), so don't load them as part of Rails.
-config.gem 'notahat-machinist', :version=>'~>0.2',
-  :source=>'http://gems.github.com',    :lib=>'machinist'
-config.gem 'rspec-rails',       :version=>'1.1.12',
-  :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'annotate',          :version=>'~>2.0.1',  # Used for annotating models with database schema (rake db:annotate)
-  :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'faker',             :version=>'~>0.3',    # Used to populate development database with fake data (rake db:populate)
-  :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'thin',              :version=>'~>1.0',    # Not essential, but development scripts (e.g. rake run) are hard wired to use Thin.
-  :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'sqlite3-ruby',      :version=>'~>1.2',    # Development and test databases use SQLite3 by default. 
-  :source=>'http://gems.rubyforge.org', :lib=>'sqlite3'
