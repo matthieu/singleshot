a9baf5b73d91c5f2c093675b7704fb3993520f18
Assaf Arkin <assaf@apache.org>
2009-02-10 00:00:35 -0800

    Added machinist Gem

--- config/environment.rb
+++ config/environment.rb
@@ -41,7 +41,6 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate',             :version=>'~> 2.3.4',
     :source=>'http://gems.github.com', :lib=>'will_paginate'
 
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -67,5 +66,4 @@ Rails::Initializer.run do |config|
 
   config.active_record.schema_format = :sql
   config.active_record.partial_updates = true
-  
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,15 +25,15 @@ config.action_mailer.raise_delivery_errors = false
 # These Gems are used exclusively during development/testing, they're listed here
 # to be included when running rake setup. Some of these are only used from rake
 # tasks or command line (e.g. rspec, thin), so don't load them as part of Rails.
-=begin
-config.gem 'rspec-rails',   :version=>'1.1.12',
+config.gem 'rspec-rails',       :version=>'1.1.12',
   :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'annotate',      :version=>'~>2.0.1',  # Used for annotating models with database schema (rake db:annotate)
+config.gem 'notahat-machinist', :version=>'~>0.2',
+  :source=>'http://gems.github.com',  :lib=>'machinist'
+config.gem 'annotate',          :version=>'~>2.0.1',  # Used for annotating models with database schema (rake db:annotate)
   :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'faker',         :version=>'~>0.3',    # Used to populate development database with fake data (rake db:populate)
+config.gem 'faker',             :version=>'~>0.3',    # Used to populate development database with fake data (rake db:populate)
   :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'thin',          :version=>'~>1.0',    # Not essential, but development scripts (e.g. rake run) are hard wired to use Thin.
+config.gem 'thin',              :version=>'~>1.0',    # Not essential, but development scripts (e.g. rake run) are hard wired to use Thin.
   :source=>'http://gems.rubyforge.org', :lib=>false
-config.gem 'sqlite3-ruby',  :version=>'~>1.2',    # Development and test databases use SQLite3 by default. 
+config.gem 'sqlite3-ruby',      :version=>'~>1.2',    # Development and test databases use SQLite3 by default. 
   :source=>'http://gems.rubyforge.org', :lib=>'sqlite3'
-=end
