f5e15d4696c3a2126de2d04ca2ed31c3ba602014
Assaf Arkin <assaf@apache.org>
2009-02-12 15:10:10 -0800

    Switched license to AGPL v3.0, pending move to new home.

--- app/models/stakeholder.rb
+++ app/models/stakeholder.rb
@@ -1,29 +1,17 @@
-# == Schema Information
-# Schema version: 20090206215123
+# Singleshot  Copyright (C) 2008-2009  Intalio, Inc
 #
-# Table name: stakeholders
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#  id         :integer         not null, primary key
-#  person_id  :integer         not null
-#  task_id    :integer         not null
-#  role       :string(255)     not null
-#  created_at :datetime        not null
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with this
-# work for additional information regarding copyright ownership.  The ASF
-# licenses this file to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
-# License for the specific language governing permissions and limitations under
-# the License.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 # Represents a stakeholder in the task.  Identifies the person and their role.
