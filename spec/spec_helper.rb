fb821098728d36e50048dc7d1da59fc2baae2643
Assaf Arkin <assaf@labnotes.org>
2009-04-07 15:34:19 -0700

    Fixed: Need to require remarkable explicitly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/autorun'
 require 'spec/rails'
 require 'machinist'
+require 'remarkable'
 require File.expand_path(File.dirname(__FILE__) + '/blueprints')
 
 
