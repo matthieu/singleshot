a738dbe4582684251727c2b2016f1cd8f0510452
Assaf Arkin <assaf@labnotes.org>
2009-05-27 16:31:16 -0700

    eager_load_paths saves the day (production)

--- vendor/plugins/presenter/rails/init.rb
+++ vendor/plugins/presenter/rails/init.rb
@@ -1,6 +1,7 @@
 path = "#{Rails.root}/app/presenters"
 # That way we're able to use everything in app/presenters.
 ActiveSupport::Dependencies.load_paths << path
+ActiveSupport::Dependencies.eager_load_paths << path
 unless File.exist?(path)
   puts "Creating #{path}"
   Dir.mkdir path
